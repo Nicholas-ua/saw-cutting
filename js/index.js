e42fa2429c9c903e7cd2dafad5337b02875616f0
Nicholas-ua <m.savych@gmail.com>
2020-04-11 20:04:38 +0300

    repare calcsz

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 
+
 function calcVs() {
     var C = document.getElementById("koef_C").value;
     var x = document.getElementById("koef_x").value;
@@ -22,12 +23,9 @@ function calcVs() {
 
     var Vs = Math.pow(60000*Math.pow(1000, x)*Nd*KKD/(C*Apopr*Math.pow(h, (1-y))*Math.pow(b, (1-q))*i*Math.pow((n*z), x)), (1/(1-x)));
     
-    document.getElementById("Vs").innerHTML = "Швидкість подачі за потужністю становить не більше " + Vs.toFixed(2) + " м/хв.";
-
-    return Vs;
+    document.getElementById("Vs").innerHTML = "Швидкість подачі за потужністю " + Vs.toFixed(2) + " м/хв.";
 }
 
-var Vs = calcVs();
 
 function calcVr() {
     var D = document.getElementById("D").value;
@@ -37,10 +35,30 @@ function calcVr() {
 }
 
 
-
 function calcSz() {
+    var C = document.getElementById("koef_C").value;
+    var x = document.getElementById("koef_x").value;
+    var y = document.getElementById("koef_y").value;
+    var q = document.getElementById("koef_q").value;
+    var Nd = document.getElementById("Nd").value;
+    var KKD = document.getElementById("KKD").value;
+    var h = document.getElementById("h").value;
+    var b = document.getElementById("b").value;
     var n = document.getElementById("n").value;
     var z = document.getElementById("z").value;
+    var i = document.getElementById("i").value;
+    var Ap = document.getElementById("A_por").value;
+    var Aw = document.getElementById("A_w").value;
+    var Ar = document.getElementById("A_r").value;
+    var Ah = document.getElementById("A_h").value;
+    var Ad = document.getElementById("A_d").value;
+    var Av = document.getElementById("A_v").value;
+
+    var Apopr = (Ap*Aw*Ar*Ah*Ad*Av).toFixed(2);
+
+    var Vs = Math.pow(60000*Math.pow(1000, x)*Nd*KKD/(C*Apopr*Math.pow(h, (1-y))*Math.pow(b, (1-q))*i*Math.pow((n*z), x)), (1/(1-x)));
+    
     var Sz = Vs * 1000/(n * z);
+    
     document.getElementById("Sz").innerHTML = "Подача на зуб становить Sz = " + Sz.toFixed(2) + " мм";
 }
